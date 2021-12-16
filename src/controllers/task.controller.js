19cf598d9dc9b3565fd5d0542c3c42b28b2d2c8b
anbreaker <antunez19@gmail.com>
2021-12-16 14:17:38 +0100

    routes complete

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -25,20 +25,117 @@ export async function createTask(req, res) {
 }
 
 export const getTask = async (req, res) => {
-  const tasks = await Task.findAll({
-    attributes: ['id', 'name', 'done', 'projectid'],
-    order: [['id', 'DESC']],
-  });
+  try {
+    const tasks = await Task.findAll({
+      attributes: ['id', 'name', 'done', 'projectid'],
+      order: [['id', 'DESC']],
+    });
 
-  res.json({
-    tasks,
-  });
+    res.json({
+      tasks,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Something goes wrong',
+      data: {},
+    });
+  }
 };
 
-export const getOneTask = (req, res) => {};
+export const getOneTask = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-export const updateTask = (req, res) => {};
+    const task = await Task.findOne({
+      attributes: ['id', 'name', 'done', 'projectid'],
+      where: { id },
+    });
 
-export const getTasksByProjets = (req, res) => {};
+    res.json(task);
+  } catch (error) {
+    console.log(error);
 
-export const deleteTask = (req, res) => {};
+    res.status(500).json({
+      message: 'Something goes wrong',
+      data: {},
+    });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, done, projectid } = req.body;
+
+    const updatedTask = await Task.findOne({
+      attributes: ['id', 'name', 'done', 'projectid'],
+      where: { id },
+    });
+
+    Task.update(
+      {
+        name,
+        done,
+        projectid,
+      },
+      {
+        where: { id },
+      }
+    );
+
+    res.json({
+      message: 'Task updated',
+      updateTask,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Something goes wrong',
+      data: {},
+    });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Task.destroy({
+      where: { id },
+    });
+
+    res.json({ message: 'Task Deleted' });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Something goes wrong',
+      data: {},
+    });
+  }
+};
+
+// TODO error!! Column!
+// get to localhost:4000/api/tasks/project/1
+export const getTasksByProjets = async (req, res) => {
+  try {
+    const { projectid } = req.params;
+
+    const tasksByProject = await Task.findAll({
+      attributes: ['id', 'name', 'done', 'projectid'],
+      where: { projectid },
+    });
+
+    res.json(tasksByProject);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Something goes wrong',
+      data: {},
+    });
+  }
+};
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,28 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createTask, getTask } from '../controllers/task.controller';
+import {
+  createTask,
+  getTask,
+  getOneTask,
+  getTasksByProjets,
+  deleteTask,
+  updateTask,
+} from '../controllers/task.controller';
 
 // /api/tasks
 router.get('/', getTask);
 
 router.post('/', createTask);
+
+// /api/tasks/:id
+router.get('/:id', getTasksByProjets);
+
+router.patch('/:id', updateTask);
+
+router.delete('/:id', deleteTask);
+
+// /api/tasks/project/:projectid
+router.get('/project/:projectid', getOneTask);
 
 export default router;
